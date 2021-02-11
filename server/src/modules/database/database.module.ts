43eee93dd0246fe696b92a816b1a7cb2eabe8edf
exbinario <migue.4to.1994@gmail.com>
2021-02-11 07:20:08 -0600

    se hicieron modificaciones para poder subir el server a firebase

---
+++ server/src/modules/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+@Module({
+    imports: [TypeOrmModule.forRoot({
+        "type": "mysql",
+        "host": "35.202.164.49",
+        "port": 3306,
+        "username": "administrador",
+        "password": "Mariobross5625.",
+        "database": "elcanchito",
+        "entities": ["dist/**/*.entity{.ts,.js}"],
+        "synchronize": true
+      })]
+})
+
+// @Module({
+//     imports: [TypeOrmModule.forRoot()]
+// })
+export class DatabaseModule {
+    constructor(private connection: Connection) {}
+
+}
